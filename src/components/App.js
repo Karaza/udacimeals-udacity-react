03d2d44656c6953a7253f2c629c9257108eac138
Sophie Kowalski <skowalski57@gmail.com>
2019-12-23 20:55:47 +0100

    combineReducers added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
   render() {
+    console.log('Props', this.props);
     return <div>Hello world</div>;
   }
 }
 
-export default App;
+function mapStateToProps({ calendar, food }) {
+  // Reformate the data
+  const dayOrder = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+  ];
+
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? food[calendar[day][meal]] : null;
+
+        return meals;
+      }, {})
+    }))
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: data => dispatch(addRecipe(data)),
+    remove: data => dispatch(removeFromCalendar(data))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
